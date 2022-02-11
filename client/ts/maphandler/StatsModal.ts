b97f59567e9f36a0414ebb8ca3c90415d7a5345e
jamieboy1337 <43399648+jamieboy1337@users.noreply.github.com>
2022-02-10 18:17:52 -0800

    Dev (#4)
    changes as of 2/10

--- client/ts/maphandler/StatsModal.ts
+++ client/ts/maphandler/StatsModal.ts
@@ -20,6 +20,7 @@ export class StatsModal {
   private manager : FeatureManager;
   private lastCollision : PolyBounds;
   private htmlModal : HTMLElement;
+  private htmlHead  : HTMLElement;
   private highlightedPoly : L.Polygon;
 
   constructor(manager: FeatureManager) {
@@ -27,9 +28,43 @@ export class StatsModal {
     this.lastCollision = null;
     this.highlightedPoly = null;
     this.htmlModal = document.getElementById("stats");
+    this.htmlHead = document.querySelector("h1");
     this.updateModal();
 
+    addEventListener("mousemove", this.handleModalPosition.bind(this));
+
     // build around an HTML element which can contain map data
+  }
+
+  handleModalPosition(e: MouseEvent) {
+    const offset = this.getModalOffset(e.clientX, e.clientY);
+
+    this.htmlModal.style.left = e.clientX + offset[0] + "px";
+    this.htmlModal.style.top = e.clientY + offset[1] + "px";
+  }
+
+  private getModalOffset(mouseX: number, mouseY: number) : [number, number] {
+    const modalSize = this.htmlModal.getBoundingClientRect();
+    const headSize  = this.htmlHead.getBoundingClientRect();
+
+    if (mouseY < headSize.y + headSize.height) {
+      // this should be sufficient
+      this.htmlModal.classList.add("hidden");
+      this.manager.getMap().removeLayer(this.highlightedPoly);
+      this.lastCollision = null;
+    }
+
+    const res : [number, number] = [16, 16];
+
+    if (mouseX + modalSize.width + 16 > window.innerWidth) {
+      res[0] = -(modalSize.width + 16);
+    }
+
+    if (mouseY + modalSize.height + 33 > window.innerHeight) {
+      res[1] = -(modalSize.height + 16);
+    }
+
+    return res;
   }
 
   handleMouseEvent(e: L.LeafletMouseEvent) {
